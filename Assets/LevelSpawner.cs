c645dc8527fcae0459ace44d8a12d68f76b899ef
ile123ile <bbravencofounder@gmail.com>
2023-10-21 16:05:01 -0400

    made the game infinite

--- Assets/LevelSpawner.cs
+++ Assets/LevelSpawner.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
+    public GameObject levelRandomizer;
+    public float offset;
+    private int _index;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnLevel(-1);
+        SpawnLevel(0);
+        _index = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float playerX = PlayerMovement.instance.transform.position.x;
+        Vector2 currSpawnLoc = GetLocForIndex(_index);
+        if (playerX > currSpawnLoc.x)
+        {
+            SpawnLevel(++_index);
+        }
+    }
+
+    Vector2 GetLocForIndex(int index)
+    {
+        Vector2 spawnLoc = transform.position;
+        spawnLoc.x += offset * index;
+        return spawnLoc;
+    }
+
+    void SpawnLevel(int index)
+    {
+        Vector2 spawnLoc = GetLocForIndex(index);
+        Instantiate(levelRandomizer, spawnLoc, transform.rotation);
     }
 }
